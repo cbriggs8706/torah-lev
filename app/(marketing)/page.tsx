bbe70fc2302c543694cce9af3f0698c07bdfe0c3
Cameron Briggs <cbriggs@highdesertmilk.com>
2025-07-28 08:54:15 -0600

    verbage

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -14,13 +14,15 @@ import Link from 'next/link'
 export default function Home() {
 	return (
 		<div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
-			<div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+			<div className="relative w-[180px] h-[180px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
 				<Image src="/hero.svg" fill alt="Hero" />
 			</div>
 			<div className="flex flex-col items-center gap-y-8">
 				<h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
-					Learn, practice, and master languages with Idiom Go&apos;s
-					comprehensible input.
+					&apos;Idiom&apos; is both a root of the word &apos;language&apos; and
+					makes up over half of everyday speech. IdiomGo uses comprehensible
+					input and engaging activities to help you hear, speak, read, and write
+					naturally—so you can confidently &apos;go&apos; with the language.
 				</h1>
 				<div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
 					<ClerkLoading>
